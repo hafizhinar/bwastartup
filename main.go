295aac42cd091801fc4c46541007dc7a99c88430
hafizhinar <hafizhin.ar.rosyiid@gmail.com>
2021-10-16 23:10:22 +0700

    fetch user endpoint

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailibility)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+	api.GET("/users/fetch", authMiddleware(authService, userService), userHandler.FetchUser)
 
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
 	api.GET("/campaigns/:id", campaignHandler.GetCampaign)
